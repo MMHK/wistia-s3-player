78fdbd088d9d4323bc18d6f46a4d625a78dbfe17
echo <echo@mixmedia.com>
2024-07-11 16:39:52 +0800

    loading & error status

--- src/WistiaPlayer.vue
+++ src/WistiaPlayer.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="video-player-wrap">
+    <div v-if="loading" class="loading-spinner"></div>
     <video ref="player" v-if="!loading" class="video-js"></video>
   </div>
 </template>
@@ -53,6 +54,7 @@ export default defineComponent({
         playbackRates: [2, 1.75, 1.5, 1.25, 1, 0.75, 0.5],
       }
     },
+
   },
 
   props: {
@@ -66,7 +68,14 @@ export default defineComponent({
     this.fetchAssets(this.id)
       .then(() => {
         this.$nextTick(() => {
-          videojs(this.$refs.player, this.options);
+          // videojs(this.$refs.player, this.options);
+          var player = videojs(this.$refs.player, this.options);
+          player.on('error', function() {
+            var errorDisplayElem = document.querySelector('.vjs-error-display .vjs-modal-dialog-content');
+            if (errorDisplayElem) {
+                errorDisplayElem.innerText = "Media not found.";
+            }
+          });
         })
       });
   },
@@ -96,13 +105,25 @@ export default defineComponent({
 <style lang="scss">
   .video-player-wrap {
     position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:before {
+      content: "";
+      padding-top: 56%; 
+    }
+
+
+    .video-js {
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    }
 
     .video-js:hover {
       .vjs-big-play-button {
         background-color: rgba(84, 187, 255, 0.7);
       }
     }
-    
     
     .video-js .vjs-big-play-button {
       width: 2.5em;
@@ -221,6 +242,33 @@ export default defineComponent({
     .vjs-menu-item {
       font-size: 1.6em;
       line-height: 1.8em;
+    }
+
+    .loading-spinner {
+      font-size: 20px;
+      width: 3em;
+      height: 3em;
+      border: 0.4em solid #f3f3f3; 
+      border-top: 0.4em solid #54bbff; 
+      border-radius: 50%;
+      animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    .video-player-wrap {
+      .loading-spinner {
+        font-size: 12px;
+      }
     }
   }
 </style>
