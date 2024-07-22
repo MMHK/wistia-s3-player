091a52e7ba4a2ed5678c46b5891cd56ff29ecfa5
MMHK <sam@mixmedia.com>
2024-07-22 15:10:24 +0800

    add inline mode

--- src/main.js
+++ src/main.js
@@ -1,12 +1,35 @@
 import { createApp } from 'vue'
 import WistiaPlayer from './WistiaPlayer.vue'
-document.querySelectorAll(".wistia_embed").forEach((el) => {
-    const matches = el.className.match(/wistia_async_([^_\ ]+)/i);
+
+const IS_DEV_SERVER = __VUE_PROD_DEVTOOLS__;
+
+const init = () => {
+    document.querySelectorAll(".wistia_embed").forEach((el) => {
+        const matches = el.className.match(/wistia_async_([^_\ ]+)/i);
+        if (matches) {
+            const id = matches[1];
+            const app = createApp(WistiaPlayer, {
+                id,
+            })
+            app.mount(el);
+        }
+    })
+}
+
+const render = (videoId) => {
+    const matches = document.querySelector(`.wistia_async_${videoId}`);
     if (matches) {
-        const id = matches[1];
         const app = createApp(WistiaPlayer, {
-            id,
+            id: videoId,
         })
-        app.mount(el);
+        app.mount(matches);
     }
-})
+}
+
+export {render};
+
+if (IS_DEV_SERVER) {
+    init();
+}
+
+export default init;
